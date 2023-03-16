eb9b4a297dea2560bb054611a0fb7d03d4625fc8
Yermek <yermek20@gmail.com>
2023-03-16 23:39:33 +0600

    Task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        System.out.println((9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,19 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char ch = 'c';
+        char ch2 = 'h';
+        char ch3 = 'a';
+        char ch4 = 'r';
+        int c = ch;
+        int h = ch2;
+        int a = ch3;
+        int r = ch4;
+
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
     }
 }
